f1695593e160ec900c1f9cbdba0ca1bd72362bd1
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-13 16:51:03 -0700

    Mad a Create Game view

---
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
