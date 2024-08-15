3a6a358776f58636e83151290f7e37ee45b58fe1
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-14 19:46:52 -0700

    Added HttpGet for edit

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -41,5 +41,16 @@ namespace CPW219_eCommerceSite.Controllers
 
             return View(g);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Game? gameToEdit = await _context.Games.FindAsync(id);
+            
+            if (gameToEdit == null)
+            {
+                return NotFound();
+            }
+            return View(gameToEdit);
+        }
     }
 }
