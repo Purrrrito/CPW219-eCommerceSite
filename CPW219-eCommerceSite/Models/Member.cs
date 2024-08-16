f431f23f519e8222df9ba9a5e4e674b2a21ac0bb
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-16 11:24:00 -0700

    Added Register View Model

--- CPW219-eCommerceSite/Models/Member.cs
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -15,4 +15,26 @@ namespace CPW219_eCommerceSite.Models
 
         public string Username { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength =6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
 }
