352d66a27105e8d98999179dade40d88d54f90a4
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-28 17:40:14 -0700

    Coded Add to car, remove from cart and added a cart sumamry page

--- CPW219-eCommerceSite/Controllers/CartController.cs
+++ CPW219-eCommerceSite/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using CPW219_eCommerceSite.Data;
 using CPW219_eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
         private readonly VideoGameContext _context;
+        private const string Cart = "Cart";
 
         public CartController(VideoGameContext context)
         {
@@ -17,16 +19,74 @@ namespace CPW219_eCommerceSite.Controllers
         {
             Game? gameToAdd = _context.Games.Where(g => g.GameId == id).SingleOrDefault();
 
-            if (gameToAdd == null) 
+            if (gameToAdd == null)
             {
                 // Game with specifed id does not exist
                 TempData["Message"] = "Sorry, that game no longer exists";
                 return RedirectToAction("Index", "Games");
             }
 
-            // ToDo: Add item to a shopping cart cookie
+            CartGameViewModel cartGame = new()
+            {
+                GameId = gameToAdd.GameId,
+                Title = gameToAdd.Title,
+                Price = gameToAdd.Price
+            };
+
+            List<CartGameViewModel> cartGames = GetExistingCarData();
+            cartGames.Add(cartGame);
+            WriteShoppingCartCookie(cartGames);
+
             TempData["Message"] = $"Added {gameToAdd.Title} to cart";
             return RedirectToAction("Index", "Games");
+        }
+
+        private void WriteShoppingCartCookie(List<CartGameViewModel> cartGames)
+        {
+            string cookieData = JsonConvert.SerializeObject(cartGames);
+
+
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
+        }
+
+        /// <summary>
+        /// Return the current list of video games in teh users shopping car cookie.
+        /// If there is no cookiem and empty list will be returned
+        /// </summary>
+        /// <returns></returns>
+        private List<CartGameViewModel> GetExistingCarData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartGameViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<CartGameViewModel>>(cookie);
+        }
+
+        public IActionResult Summary()
+        {
+            // Read shopping cart data and convert to list of view model
+            List<CartGameViewModel> cartGames  = GetExistingCarData();
+            return View(cartGames);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<CartGameViewModel> cartGames = GetExistingCarData();
+
+            CartGameViewModel? targetGame =
+                cartGames.FirstOrDefault(g => g.GameId == id);
+
+            cartGames.Remove(targetGame);
+
+            WriteShoppingCartCookie(cartGames);
+
+            return RedirectToAction(nameof(Summary));
         }
     }
 }
